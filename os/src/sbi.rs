4fe157064b43b34947d75fa6f993dfc9853a85bb
polaris <2608726255@qq.com>
2024-10-25 23:42:35 +0800

    finish: ch4

--- os/src/sbi.rs
+++ os/src/sbi.rs
@@ -2,9 +2,9 @@
 
 use core::arch::asm;
 
-const SBI_SET_TIMER: usize = 0;
+const SBI_SET_TIMER: usize = 0x54490000;
 const SBI_CONSOLE_PUTCHAR: usize = 1;
-const SBI_SHUTDOWN: usize = 8;
+const SBI_SHUTDOWN: usize = 0x54490000;
 
 /// general sbi call
 #[inline(always)]
